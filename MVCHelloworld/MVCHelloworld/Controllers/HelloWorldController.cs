5ef48f9447e002b56d8fb3dae8a8468db2165f0c
Hussen Fakhrulddin <hf556482@gmail.com>
2016-05-23 22:38:22 -0700

    init

---
+++ MVCHelloworld/MVCHelloworld/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCHelloworld.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GoHelloWorld()
+        {
+            return View("HelloWorld");
+        }
+    }
+}
